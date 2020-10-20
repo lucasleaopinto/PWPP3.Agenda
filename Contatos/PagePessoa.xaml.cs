5edb56f0356e41f16f219e91fd8a4624c87b9ddc
Lucas Pinto <lucas.pinto@dxc.com>
2020-10-19 23:53:56 -0300

    segunda commit

--- Contatos/PagePessoa.xaml.cs
+++ Contatos/PagePessoa.xaml.cs
@@ -11,12 +11,26 @@ namespace Contatos
         {
             InitializeComponent();
         }
+        
+        static String[] grupo = { "Familia", "Amigos", "Trabalho", "Comercial" };
 
         private void onSalvePessoa(object sender, EventArgs args)
         {
             Pessoa p = BindingContext as Pessoa;
-            App.Database.SaveItemAsync(p);
-            Navigation.PopAsync();
+            //App.Database.SaveItemAsync(p);
+            //Navigation.PopAsync();
+
+            Console.WriteLine("String s3 {0}.", String.IsNullOrEmpty(p.Nome));
+            if (String.IsNullOrEmpty(p.Nome))
+            {
+                DisplayAlert("Notificacao", "Preencha o nome", "ok");
+            }
+            else
+            {
+                //App.Database.SaveItemAsync(p);
+                //Navigation.PopAsync();
+
+            }
 
         }
     }
--- Contatos/Pessoa.cs
+++ Contatos/Pessoa.cs
@@ -9,6 +9,8 @@ namespace Contatos
         public uint ID { get; set; }
         public String Nome { get; set; }
         public String Telefone { get; set; }
+        public String Telefone2 { get; set; }
         public String email { get; set; }
+        public String grupo { get; set; }
     }
 }
